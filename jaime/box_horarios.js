416cb2a6c0c555745ce7eaff04357480ad54e321
Raphael Delgado <raphaelsdelgado@gmail.com>
2020-08-24 12:50:29 -0300

    Atualizacao ago/2020

--- jaime/box_horarios.js
+++
@@ -1,9 +0,0 @@
-    $(function () {
-    	$('#hoursMonday').selectpicker();
-    	$('#hoursTuesday').selectpicker();
-    	$('#hoursWednesday').selectpicker();
-    	$('#hoursThursday').selectpicker();
-    	$('#hoursFriday').selectpicker();
-    	$('#hoursSaturday').selectpicker();
-    	$('#hoursSunday').selectpicker();
-    });
--- jaime/multiselect.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-    $('#multipleSelect').selectpicker();        
-});
